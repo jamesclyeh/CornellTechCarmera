cdc51f5e554fc0d807c54ba28539fafaab4f5594
Raha Ghassemi <ghasr266@newschool.edu>
2016-10-21 11:51:28 -0400

    fixed display

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,26 +1,27 @@
 let save = document.querySelector('.save');
 let cancel = document.querySelector('.cancel');
-let info = document.querySelector('#informationWrapper');
+let info = document.querySelector('#infoWrap');
+let results = document.querySelector('#results');
 let filter = document.querySelector('.filter');
 
 save.addEventListener('click', function(){
 	
 	console.log("clicked");
-	info.style.visibility = "hidden";
-	filter.style.display = "block";
+	info.style.display = "none";
+	results.style.display = "block";
 
 });
 
 cancel.addEventListener('click', function(){
 
 	console.log("clicked");
-	info.style.visibility = "hidden";
-	filter.style.display = "block";
+	info.style.display = "none";
+	results.style.display = "block";
 });
 
 filter.addEventListener('click', function(){
 
 	console.log("clicked");
-	info.style.visibility = "visible";
-	filter.style.display = "none";
+	info.style.display = "block";
+	results.style.display = "none";
 });
