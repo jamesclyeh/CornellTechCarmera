4671985df6d78b49587c7d8896e23c7658001813
Raha Ghassemi <ghasr266@newschool.edu>
2016-10-20 23:40:41 -0400

    filter

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,8 +1,26 @@
 let save = document.querySelector('.save');
 let cancel = document.querySelector('.cancel');
+let info = document.querySelector('#informationWrapper');
+let filter = document.querySelector('.filter');
 
 save.addEventListener('click', function(){
-	let info = document.getElementByID('#informationWrapper');
+	
 	console.log("clicked");
-	info.toggleSlide();
+	info.style.visibility = "hidden";
+	filter.style.display = "block";
+
+});
+
+cancel.addEventListener('click', function(){
+
+	console.log("clicked");
+	info.style.visibility = "hidden";
+	filter.style.display = "block";
+});
+
+filter.addEventListener('click', function(){
+
+	console.log("clicked");
+	info.style.visibility = "visible";
+	filter.style.display = "none";
 });
