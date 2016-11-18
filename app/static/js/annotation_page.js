824a7cbbdabbee0319284db62c53e95c4e411fc5
James Yeh <james@tellapart.com>
2016-11-17 22:16:50 -0500

    Fix text area style.

--- app/static/js/annotation_page.js
+++ app/static/js/annotation_page.js
@@ -1,33 +1,32 @@
-$('#mark-button').click(function () {
-	$('#mark-button').toggleClass("selected-button");
-	if ($('#mark-button').hasClass("selected-button")) {
-	console.log("yo");
-	$('#tag-button').removeClass("selected-button");
-}
+$('#mark-button').click(function() {
+    $('#mark-button').toggleClass("selected-button");
+    if ($('#mark-button').hasClass("selected-button")) {
+        $('#tag-button').removeClass("selected-button");
+    }
 });
 
 
-$('#tag-button').click(function () {
-	$('#tag-button').toggleClass("selected-button");
-	if ($('#tag-button').hasClass("selected-button")) {
+$('#tag-button').click(function() {
+    $('#tag-button').toggleClass("selected-button");
+    if ($('#tag-button').hasClass("selected-button")) {
 
-	$('#mark-button').removeClass("selected-button");
+        $('#mark-button').removeClass("selected-button");
 
-}
+    }
 });
 
-$.fn.toggleText = function(t1, t2){
-  if (this.text() == t1) this.text(t2);
-  else                   this.text(t1);
-  return this;
+$.fn.toggleText = function(t1, t2) {
+    if (this.text() == t1) this.text(t2);
+    else this.text(t1);
+    return this;
 };
 
-$('#tags').click(function () {
-	$('#sug').slideToggle();
-	$('#tags span').toggleText("Tags +", "Tags -");
+$('#tags').click(function() {
+    $('#sug').slideToggle();
+    $('#tags span').toggleText("Tags +", "Tags -");
 });
 
-$('#sensor-data').click(function () {
-	$('#sensor-data-item').slideToggle();
-	$('#sensor-data span').toggleText("Sensor Data +", "Sensor Data -");
+$('#sensor-data').click(function() {
+    $('#sensor-data-item').slideToggle();
+    $('#sensor-data span').toggleText("Sensor Data +", "Sensor Data -");
 });
