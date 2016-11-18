112a073f8811a93fce5107b9d862ed429095f285
James Yeh <james@tellapart.com>
2016-11-17 21:07:02 -0500

    Add connection to annotation page.

--- app/endpoint.py
+++ app/endpoint.py
@@ -14,9 +14,12 @@ def hello():
   return render_template('index.html',
       images=tmp.images, data=tmp.data, all_tags=all_tags)
 
-@app.route("/test")
-def test():
-  return render_template('annotation.html', images=[tmp.images[0]])
+@app.route("/annotate/<image_id>")
+def annotate(image_id):
+  image_id = int(image_id)
+  for img in tmp.images:
+    if img.image_id == image_id:
+      return render_template('annotation.html', images=[img])
 
 @app.route("/filterByTag", methods=['POST'])
 def filterByTag():
